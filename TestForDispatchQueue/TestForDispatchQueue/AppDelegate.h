d915e3346557ea22f69dcacd4851b4c74e1b6b2c
liyingpeng <liyingpeng@bytedance.com>
2018-04-30 23:16:09 +0800

    add new files

---
+++ TestForDispatchQueue/TestForDispatchQueue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForDispatchQueue
+//
+//  Created by liyingpeng on 2018/4/22.
+//  Copyright © 2018年 liyingpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
