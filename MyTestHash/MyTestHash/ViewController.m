c15132d6cafa11a21546570d3ad23e98481cded1
liyingpeng <liyingpeng@bytedance.com>
2019-05-25 17:28:08 +0800

    add some test project

--- MyTestHash/MyTestHash/ViewController.m
+++ MyTestHash/MyTestHash/ViewController.m
@@ -8,6 +8,47 @@
 
 #import "ViewController.h"
 
+@interface TestObject : NSObject <NSCopying>
+
+@end
+
+@implementation TestObject
+
+- (id)copyWithZone:(NSZone *)zone {
+    return self;
+//    return [[self.class allocWithZone:zone] init];
+}
+
+//- (NSUInteger)hash {
+//    return [super hash];
+//}
+//
+//- (BOOL)isEqual:(id)object {
+//    BOOL equal = [super isEqual:object];
+//    return YES;
+//}
+
+@end
+
+@interface TestString : NSMutableString
+
+@end
+
+@implementation TestString
+
+- (id)copyWithZone:(NSZone *)zone {
+    id res = [super copyWithZone:zone];
+    BOOL equal = res == self;
+    return res;
+}
+
+- (id)mutableCopyWithZone:(NSZone *)zone {
+    id res = [super mutableCopyWithZone:zone];
+    BOOL equal = res == self;
+    return res;
+}
+@end
+
 @interface ViewController ()
 
 @end
@@ -16,7 +57,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // NSMutableDictionary
+    NSMutableDictionary *dic = @{}.mutableCopy;
+    
+    TestObject *objKey = [TestObject new];
+    TestString *stringKey = [TestString stringWithString:@"a"];
+
+    [dic setObject:@1 forKey:stringKey]; // dictionary 方法，key可以使任意实现nscopying协议对象
+    [dic setObject:@2 forKey:stringKey];
+    
+    [dic setValue:@1 forKey:@"key"]; // setvalue forkey 方法是KVC方式 key只能是字符串
 }
 
 
